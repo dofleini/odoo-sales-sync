80a480f79e490f28eb37fe76d0f1b516916b51d2
Osvel Alvarez <alvarezosvel@gmail.com>
2020-08-09 00:59:09 -0400

    [ADD] Create csv

--- google_cloud_sender/models/order_line.py
+++ google_cloud_sender/models/order_line.py
@@ -2,6 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+import csv
+import inspect
+import os
 
 
 class OrderLine(models.Model):
@@ -9,5 +12,37 @@ class OrderLine(models.Model):
 
     @api.model
     def send_resume(self):
-        print('message from cron')
+        directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        csv_path = os.path.join(directory_path, 'tabla.csv')
+
+        file = open(csv_path, 'w')
+
+        orders_lines = self.env['sale.order.line'].search([])
+
+        with file:
+
+            f_names = ['product_id', 'product_uom_qty']
+            writer = csv.DictWriter(file, fieldnames=f_names)
+            writer.writeheader()
+
+            for line in orders_lines:
+                writer.writerow(self.csv_line(line))
+
+    @staticmethod
+    def csv_line(line):
+
+        vals = {}
+
+        if line.product_id:
+            vals['product_id'] = line.product_id.name
+        else:
+            vals['product_id'] = ''
+
+        if line.product_uom_qty:
+            vals['product_uom_qty'] = line.product_uom_qty
+        else:
+            vals['product_uom_qty'] = ''
+
+        return vals
+
 
